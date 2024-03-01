e5ad1ad6d7664aa02f21d79c8d2cce6890fc8710
Quentin Guiraud <quentin@Quentin.local>
2024-03-01 12:13:10 +0100

    fin du responsive et fin du projet

--- wp-content/themes/generatepress-child/functions.php
+++ wp-content/themes/generatepress-child/functions.php
@@ -15,3 +15,27 @@ function register_my_menus() {
 	);
 }
 add_action('init', 'register_my_menus');
+
+
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+
+function add_extra_item_to_nav_menu( $items, $args ) {
+	if (is_user_logged_in() && $args->menu == 'header-menu') {
+		// $items.unshift('<li><a href="http://localhost:8888/oc-Planty/wp-admin/">Admin</a></li>');
+		$new_items = '<li><a href="' . get_admin_url() . '">Admin</a></li>' . $items;
+        // $items .= '<li><a href="http://localhost:8888/oc-Planty/wp-admin/">Admin</a></li>';
+        // $items .= '<li><a href=" . admin_url() . ">Admin</a></li>';
+		return $new_items;
+    }
+    return $items;
+}
+
+
+
+add_filter( 'wpcf7_form_elements', 'mycustom_wpcf7_form_elements' );
+
+function mycustom_wpcf7_form_elements( $form ) {
+$form = do_shortcode( $form );
+
+return $form;
+}
